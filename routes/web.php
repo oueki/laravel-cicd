4d358431679333690cb7c3d7492704ac26e9258f
Alex <ysov-p@mail.ru>
2022-05-27 00:32:46 +0700

    remove old file

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +16,3 @@ use App\Http\Controllers\UserController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Route::get('/users', [UserController::class, 'index']);
