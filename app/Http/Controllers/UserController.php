6da1caa0c95392389707a158e2ffbb96b295d4d2
Alex <ysov-p@mail.ru>
2022-05-26 23:44:27 +0700

    update controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,11 @@ class UserController extends Controller
     public function index()
     {
         return [
+<<<<<<< HEAD
             'user' => 2,
+=======
+            'user' => 1,
+>>>>>>> fcbe8b9... update controller
             'user_name' => 'Viktor'
         ];
     }
