154c5b41e7f753e248cbf94192c97c623683ad26
Alex <ysov-p@mail.ru>
2022-05-27 13:55:38 +0700

    update feature

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,14 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
     public function index()
     {
-        return [
-            'asd' => 'asdasdasdasdas'
-        ];
+        $users = User::all();
+
+        return $users;
     }
 }
